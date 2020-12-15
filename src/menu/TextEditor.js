fa294b46b14f576e1a8029ede67740d1b3b44385
Nelson Jaimes <nelsonjaimesgonzales@gmail.com>
2020-12-15 15:48:23 -0500

    Elements with styleColors

--- src/menu/TextEditor.js
+++ src/menu/TextEditor.js
@@ -4,37 +4,37 @@ import { EditorState, convertToRaw,ContentState, convertFromHTML} from "draft-js
 import { useDispatch, useSelector } from 'react-redux';
 import { startSaveNote } from '../actions/notes';
 
-
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import draftToHtml from "draftjs-to-html";
+import {stateFromHTML} from 'draft-js-import-html';
 
 const TextEditor = () =>{
   const dispatch = useDispatch();
   const {active} = useSelector( state => state.notes);
-  const {title, body} = active
-  
+  const {title, body} = active;
   console.log(body);
-  var initialState;
-        if (body=="''") {
-          console.log("Esta vacio !!! ")
-          initialState = EditorState.createEmpty();
-        }
-        else {
-          console.log("No está vacio...")
-          // const processedHTML = DraftPasteProcessor.processHTML(this.props.content);
-           const contentState = ContentState.createFromBlockArray(convertFromHTML(body));
-           //move focus to the end. 
-           initialState = EditorState.createWithContent(contentState);
-           initialState = EditorState.moveFocusToEnd(initialState);
-        }
+    const options: any = {
+      customInlineFn: (element: any, {Style, Entity}: any) => {
+          if (element.style.color) {
+              return Style ('color-' + element.style.color);
+          }
+      }
+    };
+
+  //const contentState = ContentState.createFromBlockArray(convertFromHTML(body));
+   //EditorState.createWithContent(stateFromHTML(props.text, options));
+  //var initialState = EditorState.createWithContent(contentState);
+  var content = "<p>Given <span style='color: #2a00ff;'><strong>Name</strong></span></p>"
+  var initialState = EditorState.createWithContent(stateFromHTML(body, options));
+  initialState = EditorState.moveFocusToEnd(initialState);
+          
   const [editorState, setEditorState] = useState(() => initialState,);
 
   const onEditorStateChange = (state) => {
-    setEditorState(state)
+    setEditorState(state);
     var data = draftToHtml(convertToRaw(state.getCurrentContent()));
     active.body = data;
     dispatch(startSaveNote(active));
-    console.log(data);
   };
 
    return (
